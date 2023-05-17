3dd6dfca5d2eb3c831828c3c655926ef6920587f
AnderDev513 <andersondiasleite@icloud.com>
2023-05-17 14:48:00 -0300

    html semantico correções estilização e script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function abrir() {
     document.querySelector("form").style.opacity = 1
-    document.querySelector("div").style.opacity = 0.2
+    document.querySelector("main").style.opacity = 0.2
 }
 
 function fechar() {
     document.querySelector("form").style.opacity = 0
-    document.querySelector("div").style.opacity = 1
+    document.querySelector("main").style.opacity = 1
 }
